d78e92df5c46f06216ea52855459ccf9a0aa62f5
BaronYan <1229598328@qq.com>
2024-09-04 11:32:57 +0800

    软链接

--- docs/.vuepress/.temp/pages/software/plugins/index.html.vue
+++ docs/.vuepress/.temp/pages/software/plugins/index.html.vue
@@ -1,5 +1,10 @@
 <template><div><p><a href="https://www.emmet.io/" target="_blank" rel="noopener noreferrer">emmet</a></p>
 <p><a href="https://code.visualstudio.com/docs/editor/emmet" target="_blank" rel="noopener noreferrer">visualstudio.emmet</a></p>
-</div></template>
+<h2 id="将-phpstorm-软链接到-ubuntu" tabindex="-1"><a class="header-anchor" href="#将-phpstorm-软链接到-ubuntu"><span>将 phpstorm 软链接到 ubuntu</span></a></h2>
+<div class="language-bash line-numbers-mode" data-highlighter="prismjs" data-ext="sh" data-title="sh"><pre v-pre class="language-bash"><code><span class="line"><span class="token function">sudo</span> <span class="token function">ln</span> <span class="token parameter variable">-s</span> /mnt/c/Users/yanbe/AppData/Local/Programs/Microsoft<span class="token punctuation">\</span> VS<span class="token punctuation">\</span> Code/bin/code /usr/bin/vscode</span>
+<span class="line"><span class="token function">sudo</span> <span class="token function">ln</span> <span class="token parameter variable">-s</span> /mnt/c/Program<span class="token punctuation">\</span> Files/JetBrains/PhpStorm<span class="token punctuation">\</span> <span class="token number">2022.2</span>/bin/phpstorm64.exe /usr/bin/phpstorm</span>
+<span class="line"><span class="token function">sudo</span> <span class="token function">ln</span> <span class="token parameter variable">-s</span> /mnt/c/Program<span class="token punctuation">\</span> Files/JetBrains/PhpStorm<span class="token punctuation">\</span> <span class="token number">2022.1</span>.2/bin/phpstorm64.exe /etc/bin/phpstorm</span>
+<span class="line"></span></code></pre>
+<div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div></div></template>
 
 
--- docs/software/plugins/index.md
+++ docs/software/plugins/index.md
@@ -4,3 +4,11 @@
 
 [visualstudio.emmet](https://code.visualstudio.com/docs/editor/emmet)
 
+
+## 将 phpstorm 软链接到 ubuntu
+
+```bash
+sudo ln -s /mnt/c/Users/yanbe/AppData/Local/Programs/Microsoft\ VS\ Code/bin/code /usr/bin/vscode
+sudo ln -s /mnt/c/Program\ Files/JetBrains/PhpStorm\ 2022.2/bin/phpstorm64.exe /usr/bin/phpstorm
+sudo ln -s /mnt/c/Program\ Files/JetBrains/PhpStorm\ 2022.1.2/bin/phpstorm64.exe /etc/bin/phpstorm
+```
